b1972637f6fc7061ee7c45f1fbf01cc39129d17e
Anthony Ezedinachi <ezedinachianthony@gmail.com>
2026-06-30 03:44:43 +0200

    Update auth.js

--- backend/scr/routes/auth.js
+++ backend/scr/routes/auth.js
@@ -1,1 +1,11 @@
-csffwegwrg
+import express from 'express';
+import { register, login, getProfile } from '../controllers/authController.js';
+import auth from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/profile', auth, getProfile);
+
+export default router;
