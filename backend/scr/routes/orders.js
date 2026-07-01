f0a8f3619deb5aa9a7b762010b2c39d6c1d65702
Anthony Ezedinachi <ezedinachianthony@gmail.com>
2026-07-01 08:26:11 +0200

    Update orders.js

--- backend/scr/routes/orders.js
+++ backend/scr/routes/orders.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createOrder, getOrder, updateOrderStatus } from '../controllers/orderController.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.post('/', createOrder);
-router.get('/:id', getOrder);
-router.put('/:id/status', updateOrderStatus);
+router.post('/', auth, createOrder);
+router.get('/:id', auth, getOrder);
+router.put('/:id/status', auth, updateOrderStatus);
 
 export default router;
