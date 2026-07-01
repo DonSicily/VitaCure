c7d759b71547c74734c4ba1fbb9d175f03606319
Anthony Ezedinachi <ezedinachianthony@gmail.com>
2026-07-01 07:26:17 +0200

    Create seedWithImages.js

---
+++ frontend/src/utils/seedWithImages.js
@@ -0,0 +1,5 @@
+// Add to your seed script for testing with images
+export const base64Images = {
+  ashwagandha: 'data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgNDAwIDMwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCB3aWR0aD0iNDAwIiBoZWlnaHQ9IjMwMCIgZmlsbD0iI0U4RjVFOSIgcng9IjEyIi8+PHRleHQgeD0iMjAwIiB5PSIxNTAiIGZvbnQtc2l6ZT0iMjAiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGZpbGw9IiM0Q0FGNTAiPkFzaHdhZ2FuZGhhaCA8L3RleHQ+PC9zdmc+',
+  // Add more as needed
+};
